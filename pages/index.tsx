e9a3ceed31a5b464a8dc54e9ef3d014b3985ddc8
Ernest <ernest.a.elgin.iv@gmail.com>
2022-06-27 11:02:00 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import ContainerBlock from "../components/SiteWrapper";
 import SiteWrapper from "../components/SiteWrapper";
 //import FavouriteProjects from "../components/FavouriteProjects";
 //import LatestCode from "../components/LatestCode";
