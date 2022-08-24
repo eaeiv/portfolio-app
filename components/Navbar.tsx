b875466538cfb3daa6f65e993a051a26ed8b0b2a
eelgin <eelgin@villanova.edu>
2022-08-24 10:11:21 -0400

    Adds mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -103,7 +103,23 @@ export default function Navbar() {
         </div>
       </div>
 
-        
+      <div className="space-x-8 block md:hidden mt-4">
+        <Link href="/about">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            About
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Projects
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Contact
+          </a>
+        </Link>
+      </div>
     </div>
   );
 
